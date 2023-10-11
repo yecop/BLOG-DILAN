a62ddff7ea5fca49309d73e539425d60a9ac3db8
yecop <111442239+yecop@users.noreply.github.com>
2023-10-10 23:43:32 -0500

    s

--- assets/js/pages/routes.js
+++ assets/js/pages/routes.js
@@ -7,6 +7,7 @@ export function Routes() {
     $routes.innerHTML =
     `
     <!-- Modal -->
+    <p>Hola</p>
     <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="w-100 pt-1 mb-5 text-right">
