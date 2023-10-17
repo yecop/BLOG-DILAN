16e64a24ddf20ff1e5c4b0d96c88d8861e1f715c
yecop <111442239+yecop@users.noreply.github.com>
2023-10-17 11:03:07 -0500

    final correo

--- mail/mail.php
+++ mail/mail.php
@@ -1,28 +1,15 @@
 <?php
   if (isset($_POST['enviar'])) {
-    if (!empty($_POST['name'] || !empty($_POST['email'] || !empty($_POST['phone'])||!empty($_POST['website'])||!empty($_POST['message'])))) {
-        $name=$_POST['name'];
-        $asunto=$_POST['subject'];
-        $email=$_POST['email'];
-        $message=$_POST['message'];
-        $header="From: noreply@asomarianis.org";
-        $to = "yecop123a@gmail.com";
-      $mail = mail($to,$asunto,$message,$header);
-      if ($mail) {
-        echo "SI SE ENVIÓ";
-      } else {
-        echo "NO";
-      }
-
+    if (!empty($_POST['name']&&!empty($_POST['email'])&&!empty($_POST['subject'])&&!empty($_POST['message']))) {
       $name=$_POST['name'];
-      $asunto=$_POST['subject'];
       $email=$_POST['email'];
+      $asunto=$_POST['subject'];
       $message=$_POST['message'];
       error_reporting( E_ALL );
       $from = "notificaciones@pruebas-enlace.shop";
       $to = "yecop123a@gmail.com";
       $subject = $asunto;
-      $message = "Saludos me gustaría que"."\r\n".$_POST['email'].$_POST['phone'].$_POST['website'];
+      $message = "Saludos, mi nombre es ".$name."\r\n\r\n\r\n\r\n".$message."\r\n\r\n\r\n\r\n"."mis datos de contacto son los siguientes: "."\r\n"."Email: ".$email ;
       $headers = "From:" . $from;
       mail($to,$subject,$message, $headers);
       
